99565842fa81737a2d871d228114d55fc102889c
Pavel_Shcherbakov1 <Pavel_Shcherbakov1@epam.com>
2015-10-02 17:07:25 +0300

    _5_linked_list

---
+++ src/_5_linked_list/UseMyList.java
@@ -0,0 +1,16 @@
+package _5_linked_list;
+
+public class UseMyList {
+
+    public static void main(String[] args) {
+
+        MyList<String> list = new MyList<>();
+
+        list.add("one");
+        list.add("two");
+        list.add("three");
+
+        System.out.println(list);
+    }
+
+}
