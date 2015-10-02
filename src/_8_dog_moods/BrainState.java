e0b7c96182c50245cd2286315f7590629aa8b90b
Pavel_Shcherbakov1 <Pavel_Shcherbakov1@epam.com>
2015-10-02 16:16:38 +0300

    _8_dog_moods

---
+++ src/_8_dog_moods/BrainState.java
@@ -0,0 +1,6 @@
+package _8_dog_moods;
+
+public interface BrainState {
+    void feed(Dog dog);
+    void stroke(Dog dog);
+}
---
+++ src/_8_dog_moods/CommunicateWithDog.java
@@ -0,0 +1,19 @@
+package _8_dog_moods;
+
+public class CommunicateWithDog {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.stroke();
+        dog.feed();
+        dog.stroke();
+        dog.stroke();
+        dog.stroke();
+        dog.stroke();
+        dog.stroke();
+        dog.stroke();
+    }
+
+}
