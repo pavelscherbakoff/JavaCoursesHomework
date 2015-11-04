a7b5be7ef10d17211fed5b2ce16a0cf5f6eab5ef
pavel.scherbakoff <pavel.scherbakoff@gmail.com>
2015-11-04 13:48:16 +0300

    _8_inner_iterators

---
+++ src/_8_inner_iterators/UseMyList.java
@@ -0,0 +1,31 @@
+package _8_inner_iterators;
+
+import java.util.Iterator;
+
+public class UseMyList {
+
+    public static void main(String[] args) {
+
+        MyList<String> list = new MyList<String>() {{
+            add("one");
+            add("two");
+            add("three");
+        }};
+
+        Iterator<String> anonymousIterator = list.iterator();
+        printListUsingIterator(anonymousIterator);
+
+        Iterator<String> localIterator = list.getLocalIterator();
+        printListUsingIterator(localIterator);
+
+        Iterator<String> innerIterator = list.new InnerIterator();
+        printListUsingIterator(innerIterator);
+    }
+
+    private static void printListUsingIterator(Iterator<String> it) {
+        while (it.hasNext()) {
+            String item = it.next();
+            System.out.println(item);
+        }
+    }
+}
